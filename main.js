c3ba03c66ba95dafe6da0f88ef691accdb0a2ef6
wurr05022007 <126891265+wurr05022007@users.noreply.github.com>
2024-08-29 08:48:42 -0300

    main concluído

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 const forcaSenha = document.querySelector('.forca');
 
-botoes[0].onclick = diminuiTananho;
-botoes[1].onclick = aumentaTananho;
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho() {
     if (tamanhoSenha > 1) {
@@ -28,4 +28,50 @@ function aumentaTamanho() {
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
+}
+
+for(i = 0; < checkbox.length; ++i) {
+    checkbox{i}.onclick = geraSenha;
+}
+
+geraSenha();
+
+function geraSenha() {
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1] checked) {
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3] checked) {
+        alfabeto = alfabeto + simbolos;
+    }
+    let senha= '';
+    for (let i = 0; i <tamanhoSenha; i++) {
+       let numeroAleatorio = Match.random() *alfabeto.length;
+       numeroAleatorio = Math.floor (numeroAleatorio);
+       senha = senha + alfabeto[numeroAleatorio];
+    }
+    campoSenha.Value = senha;
+    classificaSenha(alfabeto.length);
+
+}
+
+function classificaSenha(tamanhoAlfabeto){
+let entropia = tamanhoSenha * Math.log2(tamanhoSenha);
+console.log(entropia);
+forcaSenha.classList.remove('fraca', 'media', 'forte');
+if (entropia > 57) {
+    forcaSenha.classList.add('forte');
+}else if (entropia > 35 && < 57) {
+    forcaSenha.classList.add('media');
+}else if (entropia <= 35) {
+    forcaSenha.classList.add('fraca');
+}
+const valorEntropia = document.querySelector('.entropia');
+valorEntropia.textContent = "Um computador pode levar até" + Math.floor(2**entropia/(100e6*60*60*24)) + "dias para descobrir essa senha.";
 }
